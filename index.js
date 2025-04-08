3e95c2f50b8bfc13a2f7b4f11c0f825f4d7355c4
SonOfUri <alotimothy01@gmail.com>
2025-04-08 23:28:39 +0100

    update

--- index.js
+++ index.js
@@ -313,8 +313,13 @@ const axiosConfig = {
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
       "AppleWebKit/537.36 (KHTML, like Gecko) " +
       "Chrome/90.0.4430.93 Safari/537.36",
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    Referer: "https://dexscreener.com/",
   },
 };
+
 
 const getTokenPrice = async (contractAddress) => {
   try {
@@ -352,6 +357,7 @@ const getTokenImage = async (contractAddress) => {
     return null;
   }
 };
+
 
 const getFormattedTokenBalances = async (address) => {
   console.log(`Fetching token balances for wallet address: ${address}`);
